1b7bc2c4fc63be3bc8d9fde561c4bf2ba5931ca0
ghalestrilo <ghalestrilo@gmail.com>
2018-05-09 20:14:12 -0300

    Testing Name

--- src/prep.cpp
+++ src/prep.cpp
@@ -5,7 +5,6 @@ void preprocess(std::ifstream& file, source& output, bool macros){
 
     //if macro == true: Load Tables
 
-
     // Iterate through lines
         // If token (IF or EQU): Expand
         // If token (MACRO or DEFMACRO or ENDMACRO)
