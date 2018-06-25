c24a7fe9d820d03885f542444c38ea8b05afcd65
thales <ghalestrilo@gmail.com>
2018-06-25 20:08:59 -0300

    p2 data

--- Trabalho 2/p2/ligador.cpp
+++ Trabalho 2/p2/ligador.cpp
@@ -1,10 +1,17 @@
 
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
+#define vector_of_strings std::vector<std::string>
+
+bool readfile(std::string, vector_of_strings*);
+bool link(vector_of_strings, vector_of_strings, vector_of_strings*);
+
 
 int main(int argc, char const *argv[]){
     using namespace std;
-    std::vector<std::string> args;
+    vector_of_strings args;
 
     for (int i = 0; i < argc; i++)
         args.push_back(argv[i]);
@@ -12,23 +19,25 @@ int main(int argc, char const *argv[]){
     
     if (args.size() < 3) exit(-1); // arg error
 
+    std::string module1 = args[1];
+    vector_of_strings data1;
+
+    std::string module2 = args[2];
+    vector_of_strings data2;
+
+    for(auto a : args)
+        std::cout << a << ' ';
+    std::cout << std::endl << "heya" << std::endl;
+
+    // if (!readfile(module1, &data1)) exit(-2);
+    // if (!readfile(module2, &data2)) exit(-2);
 
     // Reading headers
-    std::vector<std::string> headers;
-    
-    if (headers.size() < 3) exit(-3);
-    /**
-     * 
-     * module_name = headers[0]
-     * 
-     * bitmask     = headers[3]
-    */
-    
-    // Begin Text Processing
-    std::vector<bool> bitmask;
-    std::vector<int>  text;
-    if (text.size() != bitmask.size()) exit (-2);
+    // if (data1.size() < 3) exit(-3);
+    // if (data2.size() < 3) exit(-3);
 
+    // vector_of_strings output;
+    // link (data1, data2, &output);
     return 0;
 }
 
@@ -50,3 +59,25 @@ int main(int argc, char const *argv[]){
  * T: 12 14 15 02 5 15 12 1 6 9 4 2
  * Suponha que não há 
 */
+
+
+
+
+
+bool readfile(std::string name, vector_of_strings* data){
+    data->clear();
+    
+    
+    /**
+     * 
+     * module_name = headers[0]
+     * 
+     * bitmask     = headers[3]
+    */
+
+    // Begin Text Processing
+    std::vector<bool> bitmask;
+    std::vector<int>  text;
+    if (text.size() != bitmask.size()) exit (-2);
+    return true;
+}
